b3ecd3bfd699c34c22a5c1d29a1af0f2cbf203c9
kelvindravin <kelvindravin@users.noreply.github.com>
2021-06-04 17:56:06 +0700

    [Cleaning]

--- Node Sensor A/nodeSensorA/nodeSensorA.ino
+++ Node Sensor A/nodeSensorA/nodeSensorA.ino
@@ -1,44 +1,30 @@
 // Usually COM 3
 // DHT-11
 #include "DHT.h"
-#define DHTPIN 2
-#define DHTTYPE DHT11
+DHTPIN 2
+DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
 //MQ-2
-#define         MQ_PIN                       (0)     //define which analog input channel you are going to use
-#define         RL_VALUE                     (5)     //define the load resistance on the board, in kilo ohms
-#define         RO_CLEAN_AIR_FACTOR          (9.83)  //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
-                                                     //which is derived from the chart in datasheet
- 
-/***********************Software Related Macros************************************/
-#define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
-#define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
-                                                     //cablibration phase
-#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
-#define         READ_SAMPLE_TIMES            (5)     //define the time interal(in milisecond) between each samples in 
-                                                     //normal operation
- 
-/**********************Application Related Macros**********************************/
-#define         GAS_LPG                      (0)
-#define         GAS_CO                       (1)
-#define         GAS_SMOKE                    (2)
- 
-/*****************************Globals***********************************************/
-float           LPGCurve[3]  =  {2.3,0.21,-0.47};   //two points are taken from the curve. 
-                                                    //with these two points, a line is formed which is "approximately equivalent"
-                                                    //to the original curve. 
-                                                    //data format:{ x, y, slope}; point1: (lg200, 0.21), point2: (lg10000, -0.59) 
-float           COCurve[3]  =  {2.3,0.72,-0.34};    //two points are taken from the curve. 
-                                                    //with these two points, a line is formed which is "approximately equivalent" 
-                                                    //to the original curve.
-                                                    //data format:{ x, y, slope}; point1: (lg200, 0.72), point2: (lg10000,  0.15) 
-float           SmokeCurve[3] ={2.3,0.53,-0.44};    //two points are taken from the curve. 
-                                                    //with these two points, a line is formed which is "approximately equivalent" 
-                                                    //to the original curve.
-                                                    //data format:{ x, y, slope}; point1: (lg200, 0.53), point2: (lg10000,  -0.22)                                                     
-float           Ro           =  10;                 //Ro is initialized to 10 kilo ohms
+
+//attributes
+#define MQ_PIN (0)
+#define RL_VALUE (5)
+#define RO_CLEAN_AIR_FACTOR (9.83)
+#define CALIBARAION_SAMPLE_TIMES (50)
+#define CALIBRATION_SAMPLE_INTERVAL (500)
+#define READ_SAMPLE_INTERVAL (50)
+#define READ_SAMPLE_TIMES (5)
+#define GAS_LPG (0)
+#define GAS_CO (1)
+#define GAS_SMOKE (2)
+
+//Curve for MQ-2, according to the MQ-2 documentation
+float LPGCurve[3]  =  {2.3,0.21,-0.47};
+float COCurve[3]  =  {2.3,0.72,-0.34};
+float SmokeCurve[3] = {2.3,0.53,-0.44};                            
+float Ro =  10;
 
 //Xbee
 #include <SoftwareSerial.h>
@@ -50,8 +36,8 @@ void setup() {
   
   Serial.print("Calibrating Node A Sensors...\n");  
   Serial.print( "Data transmission ready! Please wait for Node A Calibration...\n" );             
-  Ro = MQCalibration(MQ_PIN); //mulai kalibrasi MQ2 
-  dht.begin();                //mulai kalibrasi dht-11
+  Ro = MQCalibration(MQ_PIN);
+  dht.begin();
   Serial.print("Calibration is complete...\n"); 
 }
 
@@ -72,7 +58,7 @@ void loop() {
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
+  // Compute heat index in Fahrenheit (default)
   float hif = dht.computeHeatIndex(f, h);
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
--- Node Sensor B/nodeSensorB/nodeSensorB.ino
+++ Node Sensor B/nodeSensorB/nodeSensorB.ino
@@ -11,13 +11,17 @@ float adc_resolution = 1024.0;
 float correction = 2.9;
 float phAir;
 
+//Turbidity MJKDZ
+int turbidity = A1;
+float turbiditasAir;
+
 void setup() {
   Serial.begin(9600);
   xbee.begin( 9600 );
   
   Serial.print("Calibrating Node B Sensors...\n");  
   Serial.print( "Data transmission ready! Please wait for Node B Calibration...\n" ); 
-  delay(25000);
+  delay(1000);
   Serial.print("Calibration is complete...\n"); 
 }
 
@@ -28,15 +32,33 @@ void loop() {
     measurings += analogRead(pHSense);
     delay(10);
   }
-    float voltage = 5 / adc_resolution * measurings/samples;
-
-    phAir = ph(voltage);
-    Serial.println(phAir);
-    xbee.print(" P" + String(phAir));
+    float pHVoltage = 5 / adc_resolution * measurings/samples;
+    float turbidityVoltage = 5 / adc_resolution * analogRead(turbidity);
     
-    delay(10000);
+    phAir = ph(pHVoltage);
+    turbiditasAir = turbiditas(turbidityVoltage);
+    
+    Serial.println("pH : " + String(phAir));
+    Serial.println("turbiditas : " + String(turbiditasAir) + " turbiditasV : " + String(turbidityVoltage));
+    
+    xbee.print(" P" + String(phAir) + " K" + String(turbiditasAir));
+    
+    delay(1000);
 }
 
 float ph (float voltage) {
   return 7 + ((2.5 - voltage) / 0.18) + correction;
+}
+
+float turbiditas(float voltage){
+  float result;
+  result = ( -900 * voltage ) + 2980;
+  
+  if(result > 3000){
+    result = 3000;
+  }else if(result < 0){
+    result = 0;
+  }
+
+  return result;
 }
